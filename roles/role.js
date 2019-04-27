3b49933f81a9ef3052f18448867f3f0aef4d1a8e
David Schwartz <dbschwartz2@gmail.com>
2019-04-27 10:58:01 -0600

    master edited tests to reflect changes in demo site

--- roles/role.js
+++ roles/role.js
@@ -2,9 +2,8 @@ import { Role, Selector } from 'testcafe';
 
 const testRole = Role('http://localhost:8080/', async t => {
     await t
-    .click(Selector('#local-storage'))
-    .debug()
-    .expect(Selector('p').innerText).eql('https://router.vuejs.org')
-});
+    .click(Selector('#first-link'))
+    .click(Selector('#second-link'))
+}, {preserveUrl: true });
 
 export { testRole }
--- test.js
+++ test.js
@@ -10,5 +10,7 @@ fixture `A set of examples that illustrate how to use TestCafe API`
 
 // Tests
 test('Text typing basics', async t => {
+    await t.click(Selector('#second-link'))
+
     
 });
